3a1016aec1b66746ebe066e696a6b1f193afab74
Brian Dombrowski <brian@bzz.fm>
2023-01-29 10:31:04 -0600

    Add service worker

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,20 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import Astroturf from 'astroturf/vite-plugin'
+import { VitePWA as vitePWA } from 'vite-plugin-pwa'
+import mkcert from 'vite-plugin-mkcert'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const astroturf = (Astroturf as any).default()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), astroturf],
+  plugins: [
+    react(),
+    astroturf,
+    mkcert(),
+    vitePWA({ registerType: 'autoUpdate' }),
+  ],
   build: {
     rollupOptions: {
       output: {
