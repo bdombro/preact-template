3338154abc2fac47af72386f95775fac3fcf2e4e
Brian Dombrowski <brian@bzz.fm>
2023-02-10 09:26:20 -0600

    Add setMaxSize feature to maps

--- src/util/polyfills/iso/Map.ts
+++ src/util/polyfills/iso/Map.ts
@@ -15,6 +15,7 @@ export {}
 
 declare global {
   interface Map<K, V> {
+    setMaxSize(maxSize: number): void
     copy(): Map<K, V>
     toObj(): Record<string, V>
     /**
@@ -27,6 +28,17 @@ declare global {
 }
 
 Object.defineProperties(Map.prototype, {
+  /**
+   * Limit the size of the map by evicting the least-recently-used (aka LRU) items
+   *
+   * Warning: This hurts performance of map.get and map.set vs a normal map
+   */
+  setMaxSize: {
+    value: function (maxSize: number) {
+      mapApplyMaxSize(this, maxSize)
+    },
+    enumerable: false,
+  },
   copy: {
     value: function () {
       return Object.copy(this)
@@ -46,3 +58,53 @@ Object.defineProperties(Map.prototype, {
     enumerable: false,
   },
 })
+
+/**
+ * Limit the size of a map by evicting the least-recently-used (aka LRU) items
+ *
+ * Warning: This hurts performance of map.get and map.set vs a normal map
+ */
+function mapApplyMaxSize(map: any, maxSize: number) {
+  map.max = maxSize
+  map._set = map.set
+  map._get = map.get
+  map.set = (key: any, val: any) => {
+    // if key exists, delete it so it can be re-added at end of map
+    if (map.has(key)) {
+      map.delete(key)
+    } else if (map.size == map.max) {
+      // evict top of map (aka oldest)
+      map.delete(map.keys().next().value)
+    }
+
+    // Now add key to end of map
+    map._set(key, val)
+    return val
+  }
+  map.get = (key: any) => {
+    const item = map._get(key)
+    if (item) {
+      // put key at end of map
+      map.delete(key)
+      map._set(key, item)
+    }
+    return item
+  }
+}
+// Test mapApplyMaxSize
+// const assert = (b: boolean) => {
+//   if (!b) throw new Error('assertion failed')
+// }
+// const t = new Map()
+// t.setMaxSize(2)
+// t.set('a', 1)
+// t.set('b', 2)
+// t.set('a', 1)
+// t.set('c', 3) // should evict 'b'
+// assert(t.get('b') === undefined)
+// t.set('d', 4) // should evict 'a'
+// assert(t.get('a') === undefined)
+// t.get('c')
+// t.set('e', 5) // should evict 'd'
+// assert(t.get('d') === undefined)
+// console.log('mapApplyMaxSize test passed')
