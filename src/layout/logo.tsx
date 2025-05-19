a9233a5509775d979df7088b83a4f6016fd9e9cf
Brian Dombrowski <bdombro@gmail.com>
2025-05-19 12:30:30 +0200

    Added text based logo

--- src/layout/logo.tsx
+++ src/layout/logo.tsx
@@ -1,3 +1,7 @@
+/**
+ * A logo SVG with dynamic colors based on the theme.
+ * For a text-based logo, see the commented-out section below.
+ */
 export const Logo = (svgProps: SvgProps) => (
   <Svg
     height="20px"
@@ -23,3 +27,40 @@ export const Logo = (svgProps: SvgProps) => (
     </g>
   </Svg>
 )
+
+/**
+ * Below is a simlar logo but uses font text instead of SVG text. Helpful for POCs where you just want to swap the text
+ */
+// export const Logo = (svgProps: SvgProps) => (
+//   <Svg
+//     height="20px"
+//     viewBox="0 0 78 20"
+//     xmlns="http://www.w3.org/2000/svg"
+//     {...svgProps}
+//     _zx={{
+//       ...svgProps._zx,
+//       '--color': 'var(--color-primary)',
+//     }}
+//     _hover={{
+//       ...svgProps._hover,
+//       '--color': 'var(--color-primary-darker)',
+//     }}
+//   >
+//     <g fill="none">
+//       <path d="M3.768 16.141H.826V.447h73.406v2.941" stroke="var(--color)" strokeWidth={0.882} />
+//       <path d="M3.768 3.388h73.406v15.694H3.768z" fill="var(--color)" />
+//       <text
+//         x="10"
+//         y="15"
+//         fill="#fefefd"
+//         fontFamily="Arial, Helvetica, sans-serif"
+//         fontWeight="bold"
+//         fontStyle="italic"
+//         transform="scale(.9,1)"
+//         fontSize="12"
+//       >
+//         HOOKEDJS
+//       </text>
+//     </g>
+//   </Svg>
+// )
